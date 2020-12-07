c1c733e2c31a2231795742c631f6148f8a63e568
Sun2018421 <2475252204@qq.com>
2020-12-07 16:32:04 +0800

    debuging o(╥﹏╥)o

--- USER/main.c
+++ USER/main.c
@@ -8,37 +8,13 @@
 #include "ov2640.h"
 #include "dcmi.h"
 #include "usmart.h"
-//#include "sdmmc_sdcard.h"
-//#include "pcf8574.h"
-//#include "malloc.h"
-//#include "ff.h"
-//#include "exfuns.h"
-//#include "fontupd.h"
-//#include "text.h"
-//#include "piclib.h"	
-//#include "lcd.h"
-//#include "ltdc.h"
-//#include "sdram.h"
-//#include "w25qxx.h"
-//#include "nand.h"  
-//#include "mpu.h"
-/************************************************
- ALIENTEK 阿波罗STM32F7开发板 实验46
- 照相机实验-HAL库函数版
- 技术支持：www.openedv.com
- 淘宝店铺：http://eboard.taobao.com 
- 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
- 广州市星翼电子科技有限公司  
- 作者：正点原子 @ALIENTEK
-************************************************/
-
 vu8 bmp_request=0;						//bmp拍照请求:0,无bmp拍照请求;1,有bmp拍照请求,需要在帧中断里面,关闭DCMI接口.
 u8 ovx_mode=1;							//bit0:0,RGB565模式;1,JPEG模式 
 u16 curline=0;							//摄像头输出数据,当前行编号
 u16 yoffset=0;							//y方向的偏移量
 
-#define jpeg_buf_size   48*1024		//定义JPEG数据缓存jpeg_buf的大小(4M字节)->48K
-#define jpeg_line_size	1*1024			//定义DMA接收数据时,一行数据的最大值->2K
+#define jpeg_buf_size   48*1024/4		//定义JPEG数据缓存jpeg_buf的大小(4M字节)->48K
+#define jpeg_line_size	2*1024/4			//定义DMA接收数据时,一行数据的最大值->2KB
 
 u32 *dcmi_line_buf[2];					//RGB屏时,摄像头采用一行一行读取,定义行缓存  
 u32 *jpeg_data_buf;						//JPEG数据缓存buf 
@@ -48,9 +24,9 @@ volatile u8 jpeg_data_ok=0;				//JPEG数据采集完成标志
 										//0,数据没有采集完;
 										//1,数据采集完了,但是还没处理;
 										//2,数据已经处理完成了,可以开始下一帧接
-uint32_t JpegBuffer0[jpeg_line_size/4];  //2k的jpeg的缓冲内存0
-uint32_t JpegBuffer1[jpeg_line_size/4];  //2k的jpeg的缓冲内存1
-uint32_t JpegBuf[jpeg_buf_size/4];
+uint32_t JpegBuffer0[jpeg_line_size];  //2k的jpeg的缓冲内存0
+uint32_t JpegBuffer1[jpeg_line_size];  //2k的jpeg的缓冲内存1
+uint32_t JpegBuf[jpeg_buf_size];
 void REDLEDToggle(void);
 void BLUELEDToggle(void);
 //处理JPEG数据
@@ -64,17 +40,29 @@ void jpeg_data_process(void)
 	curline=yoffset;	//行数复位
 	if(ovx_mode&0X01)	//只有在JPEG格式下,才需要做处理.
 	{
-		printf("jpeg_data_ok is %d\r\n",jpeg_data_ok);
+		printf("\r\njpeg_data_ok is %d\r\n",jpeg_data_ok);
 		if(jpeg_data_ok==0)	//jpeg数据还未采集完?
 		{
       __HAL_DMA_DISABLE(&DMADMCI_Handler);//关闭DMA
       while(DMA2_Stream1->CR&0X01);	//等待DMA2_Stream1可配置 
-			rlen=jpeg_line_size-__HAL_DMA_GET_COUNTER(&DMADMCI_Handler);//得到剩余数据长度	
+			rlen=(jpeg_line_size-__HAL_DMA_GET_COUNTER(&DMADMCI_Handler));//得到剩余数据长度	
 			pbuf=jpeg_data_buf+jpeg_data_len;//偏移到有效数据末尾,继续添加
-			if(DMADMCI_Handler.Instance->CR&(1<<19))for(i=0;i<rlen;i++)pbuf[i]=dcmi_line_buf[1][i];//读取buf1里面的剩余数据
-			else for(i=0;i<rlen;i++)pbuf[i]=dcmi_line_buf[0][i];//读取buf0里面的剩余数据 
+			if(DMADMCI_Handler.Instance->CR&(1<<19))
+			{		
+				for(i=0;i<rlen;i++){
+					pbuf[i]=dcmi_line_buf[1][i];//读取buf1里面的剩余数据
+					printf("%x",pbuf[i]);
+				}
+			}
+			else 
+			{
+				for(i=0;i<rlen;i++){
+					pbuf[i]=dcmi_line_buf[0][i];//读取buf0里面的剩余数据 
+					printf("%x",pbuf[i]);
+				}
+			}
 			jpeg_data_len+=rlen;			//加上剩余长度
-			
+			printf("\r\nVSdata\r\n");
 			jpeg_data_ok=1; 				//标记JPEG数据采集完按成,等待其他函数处理
 		}
 		if(jpeg_data_ok==2)	//上一次的jpeg数据已经被处理了
@@ -94,18 +82,30 @@ void jpeg_dcmi_rx_callback(void)
 {  	
 	u16 i; 
 	u32 *pbuf;
+	u32 size = jpeg_line_size;
 	REDLEDToggle();
 	pbuf=jpeg_data_buf+jpeg_data_len;//偏移到有效数据末尾
   //  printf("jpeg_dcmi_rx_callback,data_len=%d\r\n",jpeg_data_len);
 	if(DMADMCI_Handler.Instance->CR&(1<<19))//buf0已满,正常处理buf1
 	{ 
-		for(i=0;i<jpeg_line_size;i++)pbuf[i]=dcmi_line_buf[0][i];//读取buf0里面的数据
+		printf("\r\n---------buf0-----------\r\n");
+		for(i=0;i<size;i++){
+			pbuf[i]=dcmi_line_buf[0][i];//读取buf0里面的数据
+			printf("%x",pbuf[i]);
+		}
+		
 		jpeg_data_len+=jpeg_line_size;//偏移
-	}else //buf1已满,正常处理buf0
-	{
-		for(i=0;i<jpeg_line_size;i++)pbuf[i]=dcmi_line_buf[1][i];//读取buf1里面的数据
+	}
+	else //buf1已满,正常处理buf0
+	{	
+		printf("\r\n---------buf1-----------\r\n");
+		for(i=0;i<size;i++){
+			pbuf[i]=dcmi_line_buf[1][i];//读取buf1里面的数据
+			printf("%x",pbuf[i]);
+		}
 		jpeg_data_len+=jpeg_line_size;//偏移 
 	} 
+		//DMADMCI_Handler.Instance->CR = DMADMCI_Handler.Instance->CR ^(1<<19);
     SCB_CleanInvalidateDCache();        //清除无效化DCache
 }
 
@@ -114,13 +114,8 @@ void jpeg_dcmi_rx_callback(void)
 //    其他,错误代码
 u8 ov2640_jpg_photo(u8 *pname)
 {
-	//FIL* f_jpg; 
-//	,headok=0;
-	//u8 res=0;
 	u32 i ;
-//	u32 bwr;
-//	u32 i,jpgstart,jpglen;
-	u8* pbuf;
+	u32* pbuf;
 	//todo: 更新DMA以及文件缓冲大小，输出
 	//f_jpg=(FIL *)mymalloc(SRAMIN,sizeof(FIL));	//开辟FIL字节的内存区域 
 	//if(f_jpg==NULL)return 0XFF;				//内存申请失败.
@@ -128,14 +123,10 @@ u8 ov2640_jpg_photo(u8 *pname)
 	jpeg_data_ok=0;
 	//sw_ov2640_mode();						//切换为OV2640模式 
 	OV2640_JPEG_Mode();						//JPEG模式  
-	//res = 
 	OV2640_ImageWin_Set(0,0,1600,1200);
-	//printf("OV2640_ImageWin_Set_RetValue: %d\r\n",res);
-	//res = 
 	OV2640_OutSize_Set(1600,1200);          //拍照尺寸为1600*1200
-	//printf("OV2640_OutSize_Set_RetValue: %d\r\n",res);
 	dcmi_rx_callback=jpeg_dcmi_rx_callback;	//JPEG接收数据回调函数
-	DCMI_DMA_Init((u32)dcmi_line_buf[0],(u32)dcmi_line_buf[1],jpeg_line_size/4,2,1);//DCMI DMA配置  
+	DCMI_DMA_Init((u32)dcmi_line_buf[0],(u32)dcmi_line_buf[1],jpeg_line_size,2,1);//DCMI DMA配置  
   printf("Jpeg Data as follows :\r\n");
 	while(1){
 		DCMI_Start(); 			//启动传输 
@@ -146,13 +137,13 @@ u8 ov2640_jpg_photo(u8 *pname)
 		while(jpeg_data_ok!=1);
 					
 		//发送jpeg_data_buf中的数据
-		printf("the size of JPEG is %d\r\n",jpeg_data_len);
-		pbuf=(u8*)jpeg_data_buf;
-		for(i=0;i<jpeg_data_len*4;i++)
-			printf("%x",pbuf[i]);
-		printf("\r\n-----------------------------------------------\r\n");
-		jpeg_data_ok =2 ;
-		DCMI_Stop(); 			//停止DMA搬运
+//		printf("the size of JPEG is %d\r\n",jpeg_data_len);
+//		pbuf=jpeg_data_buf;
+//		for(i=0;i<jpeg_data_len;i++)
+//			printf("%x",pbuf[i]);
+//		printf("\r\n-----------------------------------------------\r\n");
+//		jpeg_data_ok =2 ;
+//		DCMI_Stop(); 			//停止DMA搬运
 		break;
 	}
 	return 0;
